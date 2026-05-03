bcb485e337160531bd3909386bae3005d0d9473d
oscar2454-del <oscar24540@cbtis75.edu.mx>
2026-05-03 03:42:09 -0600

    C1

--- Api1.py
+++ Api1.py
@@ -1,27 +1,28 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 from db import collection
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)
-async def home():
-    with open("templates/base.html", "r", encoding="utf-8") as f:
-        return f.read()
+async def home(request: Request):
+    return templates.TemplateResponse("base.html", {"request": request})
 
 @app.get("/estudiantes")
 async def obtener_estudiantes():
-    try:
-        datos = []
+    datos = []
+    cursor = collection.find({})
 
-        cursor = collection.find({})
+    async for doc in cursor:
+        doc["_id"] = str(doc["_id"])
+        datos.append(doc)
 
-        async for doc in cursor:
-            doc["_id"] = str(doc["_id"])
-            datos.append(doc)
+    return datos
 
-        print(f"Se encontraron {len(datos)} estudiantes")
-        return datos
-
-    except Exception as e:
-        return {"error": str(e)}
+@app.post("/agregar")
+async def agregar_estudiante(nombre: str = Form(...)):
+    data = {"nombre": nombre}
+    await collection.insert_one(data)
+    return {"mensaje": "ok"}
