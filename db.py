9e25ab3d3bd58122f6c8b27d4d3b6ddd04fad423
oscar2454-del <oscar24540@cbtis75.edu.mx>
2026-05-03 04:42:38 -0600

    C no recuerdo cual sigue

--- db.py
+++ db.py
@@ -3,6 +3,10 @@ from motor.motor_asyncio import AsyncIOMotorClient
 
 MONGO_URL = os.getenv("MONGO_URL")
 
+if not MONGO_URL:
+    raise Exception("MONGO_URL no está configurado")
+
 client = AsyncIOMotorClient(MONGO_URL)
-db = client["escuela"]
-collection = db["estudiantes"]
+
+db = client["1"]         
+collection = db["MOLO"] 
